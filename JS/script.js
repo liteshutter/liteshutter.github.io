53424df3e536720bf45aea1f14ea3ba311a21da5
liteshutter <122804692+liteshutter@users.noreply.github.com>
2023-01-26 14:38:53 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,24 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active')
 }
+
+var swiper = new Swiper(".product-slider", {                
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});  
